213dc596a6da385080c5fc2dce61718c8d937ce2
zilmasarjuna12 <zilmasarjuna12@gmail.com>
2022-11-29 21:27:09 +0700

    init

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -5,6 +5,14 @@ import { Avatar } from "./Avatar";
 export default {
   title: "Design System/Avatar",
   component: Avatar,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['tiny', 'small', 'medium', 'large'],
+    },
+  },
 };
 
 export const Standard = (args) => <Avatar {...args} />;
@@ -59,3 +67,18 @@ export const Large = (args) => (
     />
   </div>
 );
+
+
+const Template = args => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+/*
+ * More on args at:
+ * https://storybook.js.org/docs/react/writing-stories/args
+ */
+Controls.args = {
+  loading: false,
+  size: 'tiny',
+  username: 'Dominic Nguyen',
+  src: 'https://avatars2.githubusercontent.com/u/263385',
+};
